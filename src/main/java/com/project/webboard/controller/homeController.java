4e5f2c467466e391e6fa41999fd5402d099f3f97
HJungIn <gjwjddls96@naver.com>
2020-08-14 11:37:43 +0900

    branchA의 main 페이지

---
+++ src/main/java/com/project/webboard/controller/homeController.java
@@ -0,0 +1,14 @@
+package com.project.webboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+}
